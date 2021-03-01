c20f2bce7dd6be869bcfabfd7511452c2910ea39
Ehsan Moradi <esn.mrd@gmail.com>
2021-03-01 06:14:17 -0500

    Initial commit

--- lib/contact_us.dart
+++ lib/contact_us.dart
@@ -1,13 +1,58 @@
 // loading required packages
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:radioramezan/theme.dart';
 
 class ContactUs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(20),
-      height: MediaQuery.of(context).size.height -
-          MediaQuery.of(context).padding.top,
+    return Material(
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/golden_mosque_30percent.png'),
+            fit: BoxFit.fitWidth,
+            alignment: Alignment.bottomCenter,
+          ),
+        ),
+        foregroundDecoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/modal_top.png'),
+              fit: BoxFit.fitWidth,
+              alignment: Alignment.topCenter),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            SizedBox(height: 60),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: Text(
+                'ارتباط با ما',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: RadioRamezanColors.ramady,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: SingleChildScrollView(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: <Widget>[
+                    // Text(snapshot.data),
+                    SizedBox(height: 20),
+                    // Image.asset('assets/images/faraj.png', scale: 2),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
