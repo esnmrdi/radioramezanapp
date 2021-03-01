e8b4e5fd1f499de028b6d8855bc6a4e7a9959a3f
Ehsan Moradi <esn.mrd@gmail.com>
2021-03-01 06:15:27 -0500

    Initial commit

--- lib/support_us.dart
+++ lib/support_us.dart
@@ -1,13 +1,131 @@
 // loading required packages
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:radioramezan/theme.dart';
 
 class SupportUs extends StatelessWidget {
+  final String paypalURL = 'https://www.paypal.com/paypalme2/RadioRamezan';
+  final String paypingURL = 'https://www.payping.ir/@radioramezan';
+
+  Future<String> loadTextAsset() async {
+    return await rootBundle.loadString('assets/texts/support_us.txt');
+  }
+
+  launchURL(url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(20),
-      height: MediaQuery.of(context).size.height -
-          MediaQuery.of(context).padding.top,
+    return Material(
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/golden_mosque_30percent.png'),
+            fit: BoxFit.fitWidth,
+            alignment: Alignment.bottomCenter,
+          ),
+        ),
+        foregroundDecoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/modal_top.png'),
+              fit: BoxFit.fitWidth,
+              alignment: Alignment.topCenter),
+        ),
+        child: FutureBuilder(
+          future: loadTextAsset(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  SizedBox(height: 60),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Text(
+                      'حمایت مالی',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: RadioRamezanColors.ramady,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: SingleChildScrollView(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: <Widget>[
+                          Text(snapshot.data),
+                          SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                height: 55,
+                                child: RaisedButton(
+                                  elevation: 2,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                  color: Colors.white,
+                                  onPressed: () {
+                                    launchURL(paypingURL);
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 5, vertical: 10),
+                                    child: Image.asset(
+                                      'assets/images/paypal.png',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                height: 55,
+                                child: RaisedButton(
+                                  elevation: 2,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                  color: Colors.white,
+                                  onPressed: () {
+                                    launchURL(paypingURL);
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 5, vertical: 10),
+                                    child: Image.asset(
+                                        'assets/images/payping.png'),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text("${snapshot.error}"),
+              );
+            }
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+      ),
     );
   }
 }
