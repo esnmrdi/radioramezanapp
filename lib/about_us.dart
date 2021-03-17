c107b71a994bcfe6edaab23387bbc48d3dba7c35
Ehsan Moradi <esn.mrd@gmail.com>
2021-03-17 04:57:21 -0400

    Complete about us

--- lib/about_us.dart
+++ lib/about_us.dart
@@ -4,72 +4,70 @@ import 'package:flutter/services.dart';
 import 'package:radioramezan/theme.dart';
 
 class AboutUs extends StatelessWidget {
-  Future<String> loadTextAsset() async {
-    return await rootBundle.loadString('assets/texts/about_us.txt');
+  Future<String> loadTextAsset(String url) async {
+    return await rootBundle.loadString(url);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/golden_mosque_30percent.png'),
+    return Container(
+      height: MediaQuery.of(context).size.height -
+          MediaQuery.of(context).padding.top,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/golden_mosque_20percent.png'),
+          fit: BoxFit.fitWidth,
+          alignment: Alignment.bottomCenter,
+        ),
+      ),
+      foregroundDecoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/images/modal_top.png'),
             fit: BoxFit.fitWidth,
-            alignment: Alignment.bottomCenter,
-          ),
-        ),
-        foregroundDecoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/images/modal_top.png'),
-              fit: BoxFit.fitWidth,
-              alignment: Alignment.topCenter),
-        ),
-        child: FutureBuilder(
-          future: loadTextAsset(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Column(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  SizedBox(height: 60),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 20),
-                    child: Text(
-                      'درباره ما',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: RadioRamezanColors.ramady,
-                      ),
+            alignment: Alignment.topCenter),
+      ),
+      child: FutureBuilder(
+        future: loadTextAsset('assets/texts/about_us.txt'),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: <Widget>[
+                SizedBox(height: 90),
+                Container(
+                  child: Text(
+                    'درباره ما',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: RadioRamezanColors.ramady,
                     ),
                   ),
-                  Expanded(
-                    flex: 1,
-                    child: SingleChildScrollView(
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      child: Column(
-                        children: <Widget>[
-                          Text(snapshot.data),
-                          SizedBox(height: 20),
-                          Image.asset('assets/images/faraj.png', scale: 2),
-                          SizedBox(height: 20),
-                        ],
-                      ),
+                ),
+                SizedBox(height: 20),
+                Expanded(
+                  flex: 1,
+                  child: SingleChildScrollView(
+                    padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                    child: Column(
+                      children: <Widget>[
+                        Text(snapshot.data),
+                        SizedBox(height: 20),
+                        Image.asset('assets/images/faraj.png', scale: 2),
+                      ],
                     ),
                   ),
-                ],
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text("${snapshot.error}"),
-              );
-            }
-            return Center(
-              child: CircularProgressIndicator(),
+                ),
+              ],
             );
-          },
-        ),
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text("${snapshot.error}"),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
