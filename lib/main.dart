982311ad328861ad02da43bd610009940aa9ebdd
Ehsan Moradi <esn.mrd@gmail.com>
2021-04-01 03:05:10 -0400

    Remove common app bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:preload_page_view/preload_page_view.dart';
 import 'package:dynamic_theme/dynamic_theme.dart';
@@ -9,7 +10,6 @@ import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 import 'package:radioramezan/splash.dart';
 import 'package:radioramezan/globals.dart';
 import 'package:radioramezan/side_drawer.dart';
-import 'package:radioramezan/top_app_bar.dart';
 import 'package:radioramezan/nav_bar.dart';
 import 'package:radioramezan/advertisements.dart';
 import 'package:radioramezan/qibla.dart';
@@ -17,13 +17,15 @@ import 'package:radioramezan/conductor.dart';
 import 'package:radioramezan/home_page.dart';
 import 'package:radioramezan/prayers.dart';
 import 'package:radioramezan/app_settings.dart';
-import 'theme.dart';
+import 'package:radioramezan/radio_bar.dart';
+import 'package:radioramezan/theme.dart';
 
 Future<Null> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Settings.init(
+  await Settings.init(
     cacheProvider: SharePreferenceCache(),
   );
+  await globals.init();
   runApp(
     RadioRamezanApp(),
   );
@@ -44,48 +46,44 @@ class RadioRamezanApp extends StatelessWidget {
     );
     return DynamicTheme(
       defaultBrightness: Brightness.light,
-      data: (brightness) => ThemeData(
-        primarySwatch: RadioRamezanColors.ramady,
-        accentColor: RadioRamezanColors.ramady,
-        fontFamily: 'Sans',
-        brightness: brightness,
-      ),
+      data: (brightness) => lightTheme,
       themedWidgetBuilder: (context, theme) {
-        return FutureBuilder(
-          future: globals.init(),
-          builder: (context, AsyncSnapshot snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return MaterialApp(
-                home: Splash(),
-                localizationsDelegates: [
-                  GlobalCupertinoLocalizations.delegate,
-                  GlobalMaterialLocalizations.delegate,
-                  GlobalWidgetsLocalizations.delegate,
-                ],
-                supportedLocales: [
-                  Locale('fa', 'IR'),
-                ],
-                locale: Locale('fa', 'IR'),
-                debugShowCheckedModeBanner: false,
-              );
-            } else {
-              return MaterialApp(
-                title: 'رادیو رمضان',
-                theme: theme,
-                home: Main(),
-                localizationsDelegates: [
-                  GlobalCupertinoLocalizations.delegate,
-                  GlobalMaterialLocalizations.delegate,
-                  GlobalWidgetsLocalizations.delegate,
-                ],
-                supportedLocales: [
-                  Locale('fa', 'IR'),
-                ],
-                locale: Locale('fa', 'IR'),
-                debugShowCheckedModeBanner: false,
-              );
-            }
-          },
+        // return FutureBuilder(
+        //   future: globals.init(),
+        //   builder: (context, AsyncSnapshot snapshot) {
+        //     return MaterialApp(
+        //       title: 'رادیو رمضان',
+        //       theme: theme,
+        //       home: snapshot.connectionState == ConnectionState.waiting
+        //           ? Splash()
+        //           : Main(),
+        //       localizationsDelegates: [
+        //         GlobalCupertinoLocalizations.delegate,
+        //         GlobalMaterialLocalizations.delegate,
+        //         GlobalWidgetsLocalizations.delegate,
+        //       ],
+        //       supportedLocales: [
+        //         Locale('fa', 'IR'),
+        //       ],
+        //       locale: Locale('fa', 'IR'),
+        //       debugShowCheckedModeBanner: false,
+        //     );
+        //   },
+        // );
+        return MaterialApp(
+          title: 'رادیو رمضان',
+          theme: theme,
+          home: Main(),
+          localizationsDelegates: [
+            GlobalCupertinoLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          supportedLocales: [
+            Locale('fa', 'IR'),
+          ],
+          locale: Locale('fa', 'IR'),
+          debugShowCheckedModeBanner: false,
         );
       },
     );
@@ -97,10 +95,10 @@ class Main extends StatefulWidget {
   final String title;
 
   @override
-  _MainState createState() => _MainState();
+  MainState createState() => MainState();
 }
 
-class _MainState extends State<Main> {
+class MainState extends State<Main> {
   @override
   void initState() {
     super.initState();
@@ -108,8 +106,8 @@ class _MainState extends State<Main> {
 
   @override
   void dispose() {
-    globals.pageController.dispose();
     globals.radioPlayer.dispose();
+    globals.pageController.dispose();
     super.dispose();
   }
 
@@ -117,45 +115,34 @@ class _MainState extends State<Main> {
   Widget build(BuildContext context) {
     return SafeArea(
       top: false,
-      // maintainBottomViewPadding: true,
-      child: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+      child: Scaffold(
+        key: globals.mainScaffoldKey,
+        drawerEnableOpenDragGesture: false,
+        drawer: SideDrawer(),
+        body: SizedBox.expand(
+          child: PreloadPageView(
+            preloadPagesCount: 4,
+            controller: globals.pageController,
+            physics: NeverScrollableScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            onPageChanged: (int index) {
+              setState(() => globals.navigatorIndex = index);
+            },
+            children: <Widget>[
+              Qibla(),
+              Conductor(),
+              HomePage(),
+              Prayers(),
+              AppSettings(),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Expanded(
-              child: Scaffold(
-                key: globals.drawerKey,
-                drawerEnableOpenDragGesture: false,
-                drawer: SideDrawer(),
-                appBar: TopAppBar(),
-                body: SizedBox.expand(
-                  child: PreloadPageView(
-                    preloadPagesCount: 4,
-                    controller: globals.pageController,
-                    physics: NeverScrollableScrollPhysics(),
-                    scrollDirection: Axis.horizontal,
-                    onPageChanged: (int index) {
-                      setState(() => globals.navigatorIndex = index);
-                    },
-                    children: <Widget>[
-                      Qibla(),
-                      Conductor(),
-                      HomePage(),
-                      Prayers(),
-                      AppSettings(),
-                    ],
-                  ),
-                ),
-                bottomNavigationBar: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    NavBar(),
-                    Advertisements(),
-                  ],
-                ),
-              ),
-            ),
+            RadioBar(),
+            NavBar(),
+            Advertisements(),
           ],
         ),
       ),
