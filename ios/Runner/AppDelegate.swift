8d00c5ec6525eb4d66885de1d98033e49696b3a6
Ehsan Moradi <esn.mrd@gmail.com>
2021-04-10 02:44:58 -0400

    Fix formatting

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,13 +11,13 @@ import flutter_downloader
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyBYyiUJS-kigrK1fePgi4yOfPIZT9QDIeg")
     GeneratedPluginRegistrant.register(with: self)
-    FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
+   FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
 
 private func registerPlugins(registry: FlutterPluginRegistry) {
-    if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
-      FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
-    }
+   if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
+     FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
+   }
 }
