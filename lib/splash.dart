23b9e0512a3cbc3e2501a4063fb7b0fa372f02e5
Ehsan Moradi <esn.mrd@gmail.com>
2021-04-17 20:02:21 -0400

    Add secondary splash for web

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,23 +1,46 @@
 // loading required packages
+import 'package:universal_html/html.dart' as html;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
+  @override
+  SplashState createState() => SplashState();
+}
+
+class SplashState extends State<Splash> {
+  @override
+  void initState() {
+    if (kIsWeb) {
+      final splashImage = html.document.getElementsByClassName('splash-img');
+      if (splashImage.isNotEmpty) {
+        splashImage.first.remove();
+      }
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       top: false,
       child: Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
+        backgroundColor: kIsWeb ? Colors.transparent : Theme.of(context).primaryColor,
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
+            children: [
               Container(
                 height: 48,
                 width: 48,
                 child: CircularProgressIndicator(
                   valueColor: AlwaysStoppedAnimation<Color>(
-                    Colors.white,
+                    kIsWeb ? Theme.of(context).primaryColor : Colors.white,
                   ),
                 ),
               ),
@@ -26,7 +49,7 @@ class Splash extends StatelessWidget {
                 'دریافت اطلاعات از سرور',
                 style: TextStyle(
                   fontFamily: 'Sans',
-                  color: Colors.white,
+                  color: kIsWeb ? Theme.of(context).primaryColor : Colors.white,
                 ),
               ),
             ],
